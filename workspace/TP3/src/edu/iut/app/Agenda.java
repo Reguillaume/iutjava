606d363c0a78d99f0fd25a55d8f1fb14a6ccb183
reguillaume <remi.lapeyre@u-psud.fr>
2015-11-17 16:50:18 +0100

    AJOUT TP3

---
+++ workspace/TP3/src/edu/iut/app/Agenda.java
@@ -0,0 +1,13 @@
+package edu.iut.app;
+
+import java.util.ArrayList;
+
+public class Agenda extends ArrayList<ExamEvent> {
+	public Agenda() {		
+	}
+	
+	public void addCheckedEvent(ExamEvent examEvent) {
+		this.add(examEvent);
+	}
+	
+}
