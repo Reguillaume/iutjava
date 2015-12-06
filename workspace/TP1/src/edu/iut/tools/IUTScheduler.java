75cb272051f46be8a80565806dfccce70dd0588c
Reguillaume <remi.lapeyre@u-psud.fr>
2015-12-06 20:55:38 +0100

    MAJ TP1

--- workspace/TP1/src/edu/iut/tools/IUTScheduler.java
+++ workspace/TP1/src/edu/iut/tools/IUTScheduler.java
@@ -1,20 +1,7 @@
 package edu.iut.tools;
 
-import java.awt.CardLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-
-import edu.iut.gui.frames.SchedulerFrame;
-import edu.iut.gui.widget.agenda.AgendaPanelFactory;
-import edu.iut.gui.widget.agenda.DayPanel;
-import edu.iut.gui.widget.agenda.MonthPanel;
-import edu.iut.gui.widget.agenda.WeekPanel;
-import edu.iut.gui.widget.agenda.AgendaPanelFactory.ActiveView;
-import edu.iut.gui.widget.agenda.ControlAgendaViewPanel;
-import edu.iut.gui.widget.agenda.WeekPanel.WeekDayNames;
-
 
 public class IUTScheduler {
 	public static void main(String[] args) {
