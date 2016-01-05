07d63ef8f10a8be26fef0ee4e4cd8c31054b670c
Reguillaume <remi.lapeyre@u-psud.fr>
2016-01-05 18:08:32 +0100

    AJOUT PROJET BD

---
+++ workspace/ProjetBD/Scripts/CreationStructureBD.sql
@@ -0,0 +1,68 @@
+--Supprime les tables et types existants.
+drop table ob_stage;
+drop table ob_entreprise;
+drop table ob_etudiant;
+drop type ob_stage_ty;
+drop type ob_etudiant_ty;
+drop type ob_entreprise_ty;
+
+--Création du type etudiant.
+create or replace type ob_etudiant_ty as object (
+id number,
+nom varchar(20),
+prenom varchar(20),
+map member function tri return number );
+/
+
+create or replace type body ob_etudiant_ty is
+map member function tri return number is
+  begin
+    return id;
+  end tri;
+end;
+/
+
+--Création du type entreprise.
+create or replace type ob_entreprise_ty as object (
+id number,
+nom varchar(20),
+ville varchar(20),
+departement varchar(20),
+map member function tri return number );
+/
+
+create or replace type body ob_entreprise_ty is
+map member function tri return number is 
+  begin
+    return id;
+  end tri;
+end;
+/
+
+--Création du type stage.
+create or replace type ob_stage_ty as object (
+id number,
+dateDebut date,
+dateFin date,
+etudiant ob_etudiant_ty,
+entreprise ob_entreprise_ty,
+map member function tri return number );
+/
+
+create or replace type body ob_stage_ty is
+map member function tri return number is
+  begin
+    return id;
+  end tri;
+end;
+/
+
+--Création des tables.
+create table ob_etudiant of ob_etudiant_ty(id primary key);
+/
+
+create table ob_entreprise of ob_entreprise_ty(id primary key);
+/
+
+create table ob_stage of ob_stage_ty(id primary key);
+/
---
+++ workspace/ProjetBD/Scripts/RemplissageBD.sql
@@ -0,0 +1,137 @@
+--Supprime le contenu des tables existantes.
+DELETE FROM ob_entreprise;
+DELETE FROM ob_etudiant;
+DELETE FROM ob_stage;
+
+--Remplissage de la table entreprise.
+INSERT INTO ob_entreprise
+VALUES(1,'BNP Paribas', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(2,'RATP', 'Palaiseau', 'Essonne');
+
+INSERT INTO ob_entreprise
+VALUES(3,'Polo Ralph Lauren', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(4,'Appartoo', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(5,'Improveus', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(6,'iKNSA', 'Cergy', 'Val-d Oise');
+
+INSERT INTO ob_entreprise
+VALUES(7,'La Meduse', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(8,'Route des Hommes', 'Paris', 'Paris');
+
+INSERT INTO ob_entreprise
+VALUES(9,'Netysoft', 'Bievres', 'Essonne');
+
+--Remplissage de la table etudiant.
+INSERT INTO ob_etudiant
+VALUES(1, 'ZAPATO', 'Michel');
+
+INSERT INTO ob_etudiant
+VALUES(2, 'DAGOBERT', 'Didier');
+
+INSERT INTO ob_etudiant
+VALUES(3, 'BOMBUT', 'Tristan');
+
+INSERT INTO ob_etudiant
+VALUES(4, 'HIERNE', 'Jerome');
+
+INSERT INTO ob_etudiant
+VALUES(5 , 'BOMBUT', 'Christian');
+
+INSERT INTO ob_etudiant
+VALUES(6, 'PAQUITO', 'Ernesto');
+
+INSERT INTO ob_etudiant
+VALUES(7, 'TRILARD', 'Thibaud');
+
+INSERT INTO ob_etudiant
+VALUES(8, 'FERASSE', 'Leonard');
+
+INSERT INTO ob_etudiant
+VALUES(9, 'BALET', 'Robert');
+
+INSERT INTO ob_etudiant
+VALUES(10, 'VALEMONTRE', 'Arthur');
+
+INSERT INTO ob_etudiant
+VALUES ('11', 'HENRY', 'Thierry');
+
+INSERT INTO ob_etudiant
+VALUES ('12', 'BOND', 'James');
+
+INSERT INTO ob_etudiant
+VALUES ('13', 'POGBA', 'Paul');
+
+INSERT INTO ob_etudiant
+VALUES ('14', 'AURIER', 'Serge');
+
+--Remplissage de la table stage
+INSERT INTO ob_stage
+SELECT 1, TO_DATE('15/09/2015','DD/MM/YY'), TO_DATE('15/11/2015','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='ZAPATO' AND et.prenom='Michel'
+AND en.nom='BNP Paribas';
+
+INSERT INTO ob_stage
+SELECT 2, TO_DATE('09/01/2016','DD/MM/YY'), TO_DATE('09/03/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='DAGOBERT' AND et.prenom='Didier'
+AND en.nom='RATP';
+
+INSERT INTO ob_stage
+SELECT 3, TO_DATE('27/12/2015','DD/MM/YY'), TO_DATE('27/02/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='BOMBUT' AND et.prenom='Tristan'
+AND en.nom='Appartoo';
+
+INSERT INTO ob_stage
+SELECT 4, TO_DATE('11/12/2015','DD/MM/YY'), TO_DATE('11/02/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='HIERNE' AND et.prenom='Jerome'
+AND en.nom='Appartoo';
+
+INSERT INTO ob_stage
+SELECT 5, TO_DATE('06/10/2015','DD/MM/YY'), TO_DATE('06/12/2015','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='BOMBUT' AND et.prenom='Christian'
+AND en.nom='Improveus';
+
+INSERT INTO ob_stage
+SELECT 6, TO_DATE('23/01/2016','DD/MM/YY'), TO_DATE('23/03/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='PAQUITO' AND et.prenom='Ernesto'
+AND en.nom='RATP';
+
+INSERT INTO ob_stage
+SELECT 7, TO_DATE('29/01/2016','DD/MM/YY'), TO_DATE('29/03/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='TRILARD' AND et.prenom='Thibaud'
+AND en.nom='iKNSA';
+
+INSERT INTO ob_stage
+SELECT 8, TO_DATE('01/01/2016','DD/MM/YY'), TO_DATE('01/03/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='FERASSE' AND et.prenom='Léonard'
+AND en.nom='La Meduse';
+
+INSERT INTO ob_stage
+SELECT 9, TO_DATE('03/02/2016','DD/MM/YY'), TO_DATE('03/05/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='BALET' AND et.prenom='Robert'
+AND en.nom='Route des Hommes';
+
+INSERT INTO ob_stage
+SELECT 9, TO_DATE('12/01/2016','DD/MM/YY'), TO_DATE('12/03/2016','DD/MM/YY'), ref(et), ref(en)
+FROM ob_etudiant et, ob_entreprise en
+WHERE et.nom='VALEMONTRE' AND et.prenom='Arthur'
+AND en.nom='Netysoft';
+
